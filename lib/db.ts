7e30570c4671124e6c1ca88f6936a05303563524
Sifan23 <sifanyg123@gmail.com>
2025-02-13 19:42:55 +0300

    db connect

--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,22 @@
 import mongoose from 'mongoose';
 
 async function dbConnect(): Promise<Connection> {
-  // get the connection string from the environment
-  const mongoURI = process.env.MONGODB_URI;
-  await mongoose.connect(mongoURI);
+
+    // check if we have a connection to the database or if it's currently connecting
+  if (mongoose.connection.readyState === 0) {
+    const mongoURI = process.env.MONGODB_URI;
+
+    if (!mongoURI) {
+      throw new Error('Please define the MONGODB_URI environment variable.');
+    }
+
+    const mongooseOpts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as MongooseOptions;
+
+    await mongoose.connect(mongoURI, mongooseOpts);
+  }
 
   // return the connection object
   return mongoose.connection;
