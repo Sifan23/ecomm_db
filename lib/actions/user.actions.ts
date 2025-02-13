44ba0556d362bc2c4c5a196d86970b220d233241
Sifan23 <sifanyg123@gmail.com>
2025-02-14 02:32:16 +0300

    multiple user creating function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -11,6 +11,18 @@ export async function createUser(name: string, email: string) {
   return user;
 }
 
+// function for taking multiple users
+type UserArray = {name:string, email:string}[]
+export async function createMultipleUsers(users: UserArray){
+  // make sure we're connected to the database
+  // before doing anything!
+  await dbConnect();
+
+  // create multiple users
+  const Users = await User.create(users);
+  return Users
+}
+
 export async function getAllUsers(){
   await dbConnect();
 
